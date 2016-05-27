46391b578702b6dc402082886abf563897c24f30
Karen <dongyy2000@gmail.com>
2016-05-27 17:08:49 -0400

    delete comment lines

--- assets/js/itemDetailsListCtrl.js
+++ assets/js/itemDetailsListCtrl.js
@@ -1,10 +1,6 @@
 ﻿app.controller('itemDetailsListCtrl', ['$scope', '$http', 'AngularJS_WCFService', function ($scope, $http, AngularJS_WCFService) {
     var self = this;
     showItem = false;
-    //wcfservice.getItemDetails();
-    //self.ItemDetails = wcfservice.itemDetails;
-    //GetItemDetails();
-    //function GetItemDetails() {
     $scope.setItem = function (item) {
         debugger;
         showItem = true;
@@ -13,15 +9,5 @@
     }
     AngularJS_WCFService.GetItemDetails().then(function (itemDetails) {
         $scope.ItemDetails = itemDetails;
-    });
-    //alert("Controller : " + AngularJS_WCFService.GetItemDetails().length);
-    //$scope.ItemDetails = AngularJS_WCFService.itemDetails;
-    //debugger;
-    //var promiseGet = AngularJS_WCFService.GetItemDetails();
-    //promiseGet.then(function (p1) {
-    //    $scope.getItemDetailDisp = p1.data;
-    //}, function (errorP1) {                 
-    //    alert("Get Item Detail from WCF error " + errorP1.Message);
-    //});
-    //}
+    });     
 }]);
--- assets/js/model.js
+++ assets/js/model.js
@@ -1,65 +1,30 @@
 ﻿app.factory("AngularJS_WCFService", ['$http', function ($http) {
-    //var wcfservice = {};
-    //var itemDetails = [];
-    //wcfservice.getItemDetails = function () {
-    //    var self = this;
-    //    return $http.get("http://localhost:30362/ItemDetailsWCF.svc/GetAll")
-    //    .then(function (response) {
-    //        self.itemDetails = response.data;
-    //    }, function (resp) {
-    //        alert("WCF service failed!");
-    //    });
-    //};
-
-    //wcfservice.AddItem = function (ItemDetail) {
-    //    $http.post("http://localhost:38439/ShoppingCartService.svc/addItemMaster", ItemDetail)
-    //    .succcess(function (data, status) {
-    //        return data;
-    //    }).error(function (data, status) {
-    //        alert("Add item failed!");
-    //    })
-    //};
-    //return wcfservice;
     var itemDetails;
     var item;
     var ret = {
         GetItem: function () {
-            //var self = this;
-            //for(var i=0; i< self.itemDetails.length; i++){
-            //    item = self.itemDetails[i];
-            //    if (item.id == parseInt(id)) {
-            //        return item;
-            //    }
-            //}
             return item;
         },
         setItem: function(singleitem){
             item = singleitem;
         },
-             GetItemDetails: function () {
-                    return $http.get("http://localhost:30362/ItemDetailsWCF.svc/GetAll")
-                        .then(function (resp) {
-                            itemDetails = resp.data;
-                            //alert(itemDetails.length);
-                        return itemDetails;
-                        //if (result.succcess) {
-                        //    itemDetails = result;
-                        //    return itemDetails;
-                        //} else {
-                        //    alert("return ItemDetails json file failed");
-                        //}
-                    }, function () {
-                        alert("WCF service failed")
-                    })
-                    } ,
-                AddItem : function(ItemDetails){
-                    $http.post("http://localhost:38439/ShoppingCartService.svc/addItemMaster", ItemDetails)
-                    .succcess(function (data, status) {
-                        return data;
-                    }).error(function (data, status) {
-                        alert("Add item failed!");
-                    })
-                }
-         }
-         return ret;
+        GetItemDetails: function () {
+            return $http.get("http://localhost:30362/ItemDetailsWCF.svc/GetAll")
+                .then(function (resp) {
+                    itemDetails = resp.data;
+                return itemDetails;
+            }, function () {
+                alert("WCF service failed")
+            })
+            } ,
+        AddItem : function(ItemDetails){
+            $http.post("http://localhost:38439/ShoppingCartService.svc/addItemMaster", ItemDetails)
+            .succcess(function (data, status) {
+                return data;
+            }).error(function (data, status) {
+                alert("Add item failed!");
+            })
+        }
+    }
+    return ret;
 }])
