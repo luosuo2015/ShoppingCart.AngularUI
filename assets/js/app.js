4c8b5561da65623e1fe741add75aad92e143edf8
Karen <dongyy2000@gmail.com>
2016-05-30 10:42:32 -0400

    merge controllers into 1 file, discard itemDetailsListCtrl.js and singleItemCtrl.js use $emit to transfer data between controllers; beautifier js formatter/ the name of var and  function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 ﻿var app = angular.module("theapp", ['ngRoute']);
 app.run(['$rootScope', 'AngularJS_WCFService', function ($rootScope, AngularJS_WCFService) {
     $rootScope.date = new Date();
+
 }]);
--- assets/js/model.js
+++ assets/js/model.js
@@ -1,24 +1,16 @@
 ﻿app.factory("AngularJS_WCFService", ['$http', function ($http) {
     var itemDetails;
     var item = {
-        Item_ID: '',
-        Item_Name: '',
-        Description: '',
-        Image_Name: '',
-        Item_Price: '',
-        AddedBy: ''
+        itemID: '',
+        itemName: '',
+        description: '',
+        imageName: '',
+        itemPrice: '',
+        addedBy: ''
     };
     var ret = {
-        getItem: function () {
-            return item;
-        },
-        setItem: function (singleitem) {
-            debugger;
-            item = singleitem;
-            // alert("setItem --" + item.Item_ID);
-        },
-        GetItemDetails: function () {
-            return $http.get("http://localhost:30362/ItemDetailsWCF.svc/GetAll")
+        getItemDetails: function () {
+            return $http.get("http://localhost:30362/ItemDetailsWCF.svc/getAll")
                 .then(function (resp) {
                     itemDetails = resp.data;
                     return itemDetails;
@@ -26,8 +18,8 @@
                     alert("WCF service failed")
                 })
         },
-        AddItem: function (ItemDetails) {
-            $http.post("http://localhost:38439/ShoppingCartService.svc/addItemMaster", ItemDetails)
+        addItem: function (ItemDetails) {
+            $http.post("http://localhost:30362/ItemDetailsWCF.svc/addItemMaster", ItemDetails)
             .succcess(function (data, status) {
                 return data;
             }).error(function (data, status) {
